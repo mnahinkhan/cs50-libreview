f7d8f42c4c60f521df50763fca8ba0fe5b9cae58
Nahin <mnk1@andrew.cmu.edu>
2020-06-11 09:39:34 +0300

    implemented import function for populating book database

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,26 +20,57 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-logged_in = False
 
 @app.route("/")
-def index():
-    return render_template("index.html",logged_in=logged_in)
+def index(login_click=False, register_click=False, register_success=False, is_error=False,
+          error_msg="", username="", name="", password=""):
+
+    return render_template("index.html", logged_in=("logged_in" in session), login_click=login_click,
+                           register_click=register_click, register_success=register_success, is_error=is_error,
+                           error_msg=error_msg, username=username, name=name, password=password)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return index()
+    if request.method == "POST":
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
-@app.route("/log_in")
+        # Is the username appropriate?
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone() is not None:
+            return index(register_click=True,  is_error=True, error_msg="This username is already taken, please pick "
+                                                                        "another one", name=name)
+
+        if password1 == password2:
+            db.execute("INSERT INTO users (username, password, name) VALUES (:username, :password, :name)",
+                       {"username": username, "password": password1, "name": name})
+            db.commit()
+            return index(register_success=True)
+        else:
+            return index(register_click=True,  is_error=True, error_msg="Your passwords don't match!",
+                         username=username, name=name)
+    return index(register_click=True)
+
+
+@app.route("/log_in", methods=["GET", "POST"])
 def log_in():
-	global logged_in
-	logged_in = True
-	return index()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if db.execute("SELECT * FROM users WHERE username = :username AND password = :password",
+                      {"username": username, "password": password}).fetchone() is not None:
+            session["logged_in"] = True
+            return index()
+        else:
+            return index(login_click=True, is_error=True, error_msg="Username or password incorrect!",
+                         username=username, password=password)
+
+    return index(login_click=True)
 
 
 @app.route("/log_out")
 def log_out():
-	global logged_in
-	logged_in = False
-	return index()
+    del session['logged_in']
+    return index()
